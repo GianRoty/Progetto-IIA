b599e5abb40663598d839f0db137e9061cb16475
GianRoty <123582204+GianRoty@users.noreply.github.com>
2025-01-26 23:42:58 +0100

    Generated word cloud

--- src/ngram.py
+++ src/ngram.py
@@ -1,6 +1,6 @@
 from nltk.corpus import stopwords
 
-def generate_n_grams(text, ngram=1):
+def generate_n_grams(text, ngram = 3):
     words = [word for word in text.split(" ") if word not in set(stopwords.words('english'))]
     temp = zip(*[words[i:] for i in range(0, ngram)])
     ans = [' '.join(ngram) for ngram in temp]
--- src/train.py
+++ src/train.py
@@ -2,29 +2,27 @@ import pandas as pd
 import preprocessor as pp
 import extractor_kw as kw
 import ngram as ng
+import showing_details as sd
 
 df = pd.read_csv('../dataset/dataset.csv', sep=';')
 
-print("=========================================================")
 print("Preprocessing text")
-print("=========================================================")
 
 for i in range(df['text'].count()):
     preprocessing = pp.preprocess_text(df['text'][i])
     print(preprocessing)
 
-print("=========================================================")
-print("Extraction keywords")
-print("=========================================================")
+print("\nExtraction keywords")
 
 for i in range(df['text'].count()):
     extractKeyWords = kw.extract_kw(df['text'][i])
     print(extractKeyWords)
 
-print("==========================================================")
-print("Stemming/Lemmatization as well as n-grams")
-print("==========================================================")
+print("\nGenerating n-grams")
+
 for i in range(df['text'].count()):
     answer = ng.generate_n_grams(df['text'][i])
     print("Sentence after removing stopwords = {}".format(answer))
-    print("==========================================================")
+
+for i in range(df['text'].count()):
+    answer = sd.generate_wordcloud(df['text'][i])
