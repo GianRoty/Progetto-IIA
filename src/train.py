fed3bd7945c2f6b4381e31f365498c6fd4fd617c
GianRoty <123582204+GianRoty@users.noreply.github.com>
2025-01-26 22:42:05 +0100

    Implemented keywords extraction

--- src/train.py
+++ src/train.py
@@ -1,8 +1,20 @@
 import pandas as pd
 import preprocessor as pp
+import extractor_kw as kw
 
 df = pd.read_csv('../dataset/dataset.csv', sep=';')
+
+print("=========================================================")
+print("Preprocessing text")
+print("=========================================================")
 
 for i in range(df['text'].count()):
     preprocessing = pp.preprocess_text(df['text'][i])
     print(preprocessing)
+
+print("=========================================================")
+print("Extraction keywords")
+print("=========================================================")
+for i in range(df['text'].count()):
+    extractKeyWords = kw.extract_kw(df['text'][i])
+    print(extractKeyWords)
