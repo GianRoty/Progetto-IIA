0b022483a2122e9fc306195a381d1038b4940eca
GianRoty <123582204+GianRoty@users.noreply.github.com>
2025-01-27 21:36:15 +0100

    Fixed wordcloud issue: represented only keywords

---
+++ src/comparator.py
@@ -0,0 +1,8 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+def compare(keywords, topic):
+    vectorizer = TfidfVectorizer()
+    tfidf = vectorizer.fit_transform([" ".join(keywords), " ".join(topic)])
+    similarities = cosine_similarity(tfidf[0], tfidf[1])
+    return similarities[0][0]
--- src/train.py
+++ src/train.py
@@ -3,26 +3,60 @@ import preprocessor as pp
 import extractor_kw as kw
 import ngram as ng
 import showing_details as sd
+import comparator as cmp
 
 df = pd.read_csv('../dataset/dataset.csv', sep=';')
 
+print("\nStep 1: Finding keywords (based on article text)\n")
 print("Preprocessing text")
-
 for i in range(df['text'].count()):
     preprocessing = pp.preprocess_text(df['text'][i])
     print(preprocessing)
 
 print("\nExtraction keywords")
-
+keywordsTextTable = []
 for i in range(df['text'].count()):
-    extractKeyWords = kw.extract_kw(df['text'][i])
-    print(extractKeyWords)
+    extractKeyWordsArticleText = kw.extract_kw(df['text'][i])
+    keywordsTextTable.append(extractKeyWordsArticleText)
+print(keywordsTextTable)
 
 print("\nGenerating n-grams")
-
 for i in range(df['text'].count()):
     answer = ng.generate_n_grams(df['text'][i])
     print("Sentence after removing stopwords = {}".format(answer))
 
-for i in range(df['text'].count()):
-    answer = sd.generate_wordcloud(df['text'][i])
+keywordsText = ""
+for i in range(len(keywordsTextTable)):
+    for j in range(len(keywordsTextTable[i])):
+        keywordsText = keywordsText + " " + keywordsTextTable[i][j]
+        print(keywordsText)
+
+answer = sd.generate_wordcloud(keywordsText)
+
+print("\nStep 2: Topic analysis (based on article title)\n")
+print("Preprocessing text")
+for i in range(df['article'].count()):
+    preprocessing = pp.preprocess_text(df['article'][i])
+    print(preprocessing)
+
+print("\nExtraction keywords")
+keywordsTitleTable = []
+for i in range(df['article'].count()):
+    extractKeyWordsArticleTitle = kw.extract_kw(df['article'][i])
+    keywordsTitleTable.append(extractKeyWordsArticleTitle)
+
+print(keywordsTitleTable)
+
+print("\nGenerating n-grams")
+for i in range(df['article'].count()):
+    answer = ng.generate_n_grams(df['article'][i])
+    print("Sentence after removing stopwords = {}".format(answer))
+
+keywordsTitle = ""
+for i in range(len(keywordsTitleTable)):
+    for j in range(len(keywordsTitleTable[i])):
+        keywordsTitle = keywordsTitle + " " + keywordsTitleTable[i][j]
+
+answer = sd.generate_wordcloud(keywordsTitle)
+
+print("\nComparing keywords and article text")
